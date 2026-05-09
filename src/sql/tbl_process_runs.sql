3a9a7aade77f4fedee3c8b60687818120d41b2d7
Alice Schmidt <alicesch@ymail.com>
2026-05-08 21:14:00 -0300

    chore: remove unused files

--- src/sql/tbl_process_runs.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE public.process_runs (
-    log_id SERIAL PRIMARY KEY,
-    process_name TEXT NOT NULL,        -- ex: update_invoices, update_payments
-    last_updt_time TIMESTAMP NOT NULL DEFAULT now(),
-    info TEXT                          -- description
-);
